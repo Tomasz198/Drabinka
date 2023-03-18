ec66e81fd132581fca0a39841cdbd62fc875dfd4
Tomek <Tomek@LAPTOP-LN86A4AV>
2023-03-18 09:11:30 +0100

    Add project files.

---
+++ Drabinka/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Lets_Code_Somethings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę:");
+            int n;
+            n= Int32.Parse(Console.ReadLine()); 
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n;j++)
+                {
+                    if(i%2==0)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        if (j == 0 || j == n-1)
+                        {
+                            Console.Write("X");
+                        }
+                        else
+                        {
+                            Console.Write("-");
+                        }
+                       
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
